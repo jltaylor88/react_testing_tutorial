c841ade69b27ec245a1e4f4c5e494e93725a169c
Jason Taylor <jasonleetaylor88@gmail.com>
2021-07-07 11:26:36 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CommentBox from "components/CommentBox";
+import CommentLlist from "components/CommentList";
+
+const App = () => {
+	return (
+		<div>
+			<CommentBox />
+			<CommentLlist />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,19 @@
+import { shallow } from "enzyme";
+import React from "react";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
+let wrapped;
+
+beforeEach(() => {
+	wrapped = shallow(<App />);
+});
+
+it("shows a comment box", () => {
+	expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment List", () => {
+	expect(wrapped.find(CommentList).length).toEqual(1);
+});
